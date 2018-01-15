b2b38250c43b815352422f9a1ef11b90ef323cd6
Ayomikun Oluwayemi <oluwayemiayo@yahoo.com>
2018-01-15 12:43:19 +0100

    Working GUI for the application compiled with VS

---
+++ tic-tac-toe GUI 1.0/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace tic_tac_toe_GUI_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.turnToPlay = new System.Windows.Forms.TextBox();
+            this.C3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToPlayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // turnToPlay
+            // 
+            this.turnToPlay.Enabled = false;
+            this.turnToPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turnToPlay.Location = new System.Drawing.Point(12, 37);
+            this.turnToPlay.Multiline = true;
+            this.turnToPlay.Name = "turnToPlay";
+            this.turnToPlay.Size = new System.Drawing.Size(159, 31);
+            this.turnToPlay.TabIndex = 13;
+            // 
+            // C3
+            // 
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3.Location = new System.Drawing.Point(171, 202);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(66, 63);
+            this.C3.TabIndex = 4;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // B3
+            // 
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.Location = new System.Drawing.Point(171, 138);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(66, 63);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.Location = new System.Drawing.Point(171, 74);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(66, 63);
+            this.A3.TabIndex = 6;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.Location = new System.Drawing.Point(39, 202);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(66, 63);
+            this.C1.TabIndex = 7;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.Location = new System.Drawing.Point(105, 202);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(66, 63);
+            this.C2.TabIndex = 8;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(39, 138);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(66, 63);
+            this.B1.TabIndex = 9;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(105, 138);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(66, 63);
+            this.B2.TabIndex = 10;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(39, 74);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(66, 63);
+            this.A1.TabIndex = 11;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(105, 74);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(66, 63);
+            this.A2.TabIndex = 12;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.versionToolStripMenuItem.Text = "Version";
+            this.versionToolStripMenuItem.Click += new System.EventHandler(this.versionToolStripMenuItem_Click);
+            // 
+            // howToPlayToolStripMenuItem
+            // 
+            this.howToPlayToolStripMenuItem.Name = "howToPlayToolStripMenuItem";
+            this.howToPlayToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.howToPlayToolStripMenuItem.Text = "How to Play";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.howToPlayToolStripMenuItem,
+            this.versionToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(287, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 299);
+            this.Controls.Add(this.turnToPlay);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox turnToPlay;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToPlayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ tic-tac-toe GUI 1.0/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe_GUI_1._0
+{
+    public partial class Form1 : Form
+    {
+       
+
+        bool turn = true; // true = X turn false=Y trun
+        int turnCount = 0;
+        int xHasWon = 0;
+        int oHasWon = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            turnToPlay.Text = "Its \"X\"s turn to play";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+                // after playing for X display message that its O's turn to play
+                turnToPlay.Text = "Its \"O\"s turn to play";
+            }
+            else
+            {
+                b.Text = "O";
+                //After playing for O display message that its X's turn to play
+                turnToPlay.Text = "Its \"X\"s turn to play";
+            }
+            turn = !turn;
+            b.Enabled = false;
+            checkForWinner();
+            turnCount++;
+            
+        }
+
+        private void versionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version 1.0, \n The Initiator!");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a tic tac toe game created and designed by Phinix Works.\n Software engineer in charge: Ayomikun Oluwayemi");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // MessageBox.Show(" \"X\" gets the first chance to play");
+        }
+
+        private void checkForWinner()
+        {
+            bool thereIsAWinner = false;
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (A1.Enabled == false))
+                thereIsAWinner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (B1.Enabled == false))
+                thereIsAWinner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                thereIsAWinner = true;
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (A1.Enabled == false))
+                thereIsAWinner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (A2.Enabled == false))
+                thereIsAWinner = true;
+            else if ((A3.Text == B3.Text) && (A3.Text == C3.Text) && (!C3.Enabled))
+                thereIsAWinner = true;
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                thereIsAWinner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+                thereIsAWinner = true;
+
+            if (thereIsAWinner)
+            {
+                dissablebutton();
+                
+              
+                string winner = "";
+                if (turn) {
+                    //if true that means it is X turn to play that means O won the game
+                    winner = "O";
+                    turnToPlay.Text = "Player " + winner + " has won the game";
+                    oHasWon++;
+                }
+                else {
+                    winner = "X";
+                    turnToPlay.Text = "Player " + winner + " has won the game";
+                    xHasWon++;
+                }
+
+                MessageBox.Show("Player with marker \"" + winner + "\" is the winner \n\n X has won: "+xHasWon+" time(s) \n O has won: "+oHasWon+" time(s)");
+               
+            }
+            else
+            {
+                if (turnCount == 8)
+                {
+                    MessageBox.Show("This game ends in a draw \n\n X has won: " + xHasWon + " time(s) \n O has won: " + oHasWon + " time(s)");
+                    
+                }
+            }
+        }//this is where we check for winners after every move and print winner marker out
+
+        private void dissablebutton()
+        {
+            A1.Enabled = false;
+            A2.Enabled = false;
+            A3.Enabled = false;
+            B1.Enabled = false;
+            B2.Enabled = false;
+            B3.Enabled = false;
+            C1.Enabled = false;
+            C2.Enabled = false;
+            C3.Enabled = false;
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // for a new game session
+            turnCount = 0;
+            turn = true;
+
+            newGame();
+        }
+
+
+        private void newGame()
+        {
+            A1.Enabled = true;
+            A1.Text = "";
+            A2.Enabled = true;
+            A2.Text = "";
+            A3.Enabled = true;
+            A3.Text = "";
+            B1.Enabled = true;
+            B1.Text = "";
+            B2.Enabled = true;
+            B2.Text = "";
+            B3.Enabled = true;
+            B3.Text = "";
+            C1.Enabled = true;
+            C1.Text = "";
+            C2.Enabled = true;
+            C2.Text = "";
+            C3.Enabled = true;
+            C3.Text = "";
+            turnToPlay.Text = "Its \"X\"s turn to play";
+        }
+        private void turnToPlay_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+            // this is just to close the whole game.
+            // we would add a database soon to keep track of every players moves, and also keep an up to date scoreboard.
+
+        }
+    }
+}
